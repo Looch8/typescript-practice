654421d92a5439081b2913d2d7dd286b8e993f60
Luke Wheldale <lukewheldale@gmail.com>
2024-06-27 17:18:11 +0930

    conditonal rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,6 +4,7 @@ function ListGroup() {
 	return (
 		<>
 			<h2>List</h2>
+			{items.length === 0 && <p>No items found</p>}
 			<ul className="list-group">
 				{items.map((item) => (
 					<li key={item} className="list-group-item">

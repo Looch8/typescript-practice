50348eaadc08601198aeb6b0639b9bd8217b047c
Luke Wheldale <lukewheldale@gmail.com>
2024-06-28 17:08:21 +0930

    managing state

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,20 +1,27 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
 	let items = ["New York", "Chicago", "California", "Nebraska"];
 
-	const handleClick = (event: MouseEvent) => console.log(event);
+	const [selectedIndex, setSelectedIndex] = useState(-1);
 
 	return (
 		<>
 			<h2>List</h2>
 			{items.length === 0 && <p>No items found</p>}
 			<ul className="list-group">
-				{items.map((item) => (
+				{items.map((item, index) => (
 					<li
 						key={item}
-						onClick={handleClick}
-						className="list-group-item"
+						onClick={() => {
+							setSelectedIndex(index);
+							console.log(selectedIndex);
+						}}
+						className={
+							selectedIndex === index
+								? "list-group-item active"
+								: "list-group-item"
+						}
 					>
 						{item}
 					</li>
